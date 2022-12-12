868d44ad48713697106eecc591763d99a79fc4ee
Kai Huebl <kai@huebl-sgh.de>
2022-12-12 23:09:10 +0100

    fix ci file

--- tools/ci.sh
+++ tools/ci.sh
@@ -284,7 +284,7 @@ function unit_tests_encryption_mbedtls_pubsub {
 }
 
 function unit_tests_pubsub_sks {
-    /kdir -p build; cd build; rm -rf *
+    mkdir -p build; cd build; rm -rf *
     cmake -DCMAKE_BUILD_TYPE=Debug \
           -DUA_NAMESPACE_ZERO=FULL \
           -DUA_BUILD_EXAMPLES=ON \
